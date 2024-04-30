ed1962f0e2828d2c1997d60365e1885ee480dafd
Alexander McFarland <alex.925@gmail.com>
2024-04-30 16:06:28 -0400

    test compartmentalization
    split files into two folders for unit tests and end to end tests so they can be specified for automated coverage workflow.

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,9 +2,17 @@ conda activate bar_project_env
 
 cd /data/bar_lab/Bartender
 
+coverage -m pytest -sv  tests/end_to_end/test_ete2.py::test_run1_and_run2_run3_run4
+
+coverage run -m pytest -sv tests/test_*.py && coverage report -m
+
 python -m slipcover -m pytest -sv  tests/end_to_end/test_ete2.py::test_run1_and_run2_run3_run4
 
-python -m slipcover -m pytest -sv tests/test_*.py
+python -m slipcover -m pytest -sv tests/unit/test_*.py
+
+pytest tests/end_to_end --cov
+
+pytest tests/unit --cov
 
 # python -m slipcover -m pytest -sv tests/test_StandardBarcodeName.py
 
